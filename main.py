8aca8b309b96bb0ef0930b270dc7c7a6a43e2f6c
Kaitlyn Rice <kaitnrice@gmail.com>
2025-09-28 23:58:15 -0700

    update CLI

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-from chores import complete_chore, list_chores, init_chores, reset_all_chores, rotate_chores, seed_chores
+from chores import complete_chore, get_chore, get_chores, init_chores, reset_all_chores, rotate_chores, seed_chores
 from db import get_all
-from roommates import complete_chore_for_roommate, list_roommates, init_roommates, seed_roommates
+from roommates import complete_chore_for_roommate, get_roommate, get_roommates, init_roommates, seed_roommates
 from utils import isSunday
 
-init_chores()
-seed_chores()
-
-print('\n')
-
-init_roommates()
-seed_roommates()
-
-def print_chores():
-    rows = list_chores()
+def list_chores():
+    rows = get_chores()
 
     if not rows:
         print("~ No chores found")
@@ -20,11 +12,11 @@ def print_chores():
 
     print("\n--- All Chores ---")
     for row in rows:
-        name, frequency, id, score = row
-        print(f"[Group {id}] ({frequency}) {name} | Score: {score}")
+        id_, name, frequency, roommate_id, score, is_complete = row
+        print(f"[{id_}] Group {roommate_id} | {name} ({frequency}) | Completed: {is_complete} | Score: {score}")
 
-def print_roommates():
-    rows = list_roommates()
+def list_roommates():
+    rows = get_roommates()
 
     if not rows:
         print("~ No roommates found")
@@ -32,10 +24,10 @@ def print_roommates():
 
     print("\n--- All Roommates ---")
     for row in rows:
-        name, id, score = row
-        print(f"{name} [ID {id}] | Score: {score}")
+        id_, name, score = row
+        print(f"[{id_}] | Score: {score} | {name}")
 
-def print_all():
+def list_all():
     rows = get_all()
 
     if not rows:
@@ -47,21 +39,68 @@ def print_all():
         chore, roommate, score, isComplete = row
         print(f"{roommate} - {score} | {chore} {isComplete}")
 
+def help():
+    print("\nAvailable Commands:")
+    print("-------------------")
+    print(" a : List all items")
+    print(" c : List chores")
+    print(" r : List roommates")
+    print(" u chore_id roommate_id : Complete chore for roommate")
+    print(" h : Show this help menu")
+    print(" q : Quit the program")
 
-# print_chores()
-# print_roommates()
-print_all()
+# list_chores()
+# list_roommates()
+# list_all()
 
-print('\n\n')
-complete_chore_for_roommate(1,1)
-print_all()
+# print('\n\n')
+# complete_chore_for_roommate(1,1)
+# list_all()
 
 # print('\n\n')
 # reset_all_chores()
-# print_all()
+# list_all()
 
 # print('\n\n')
 # rotate_chores()
-# print_all()
+# list_all()
 
 # print(isSunday())
+
+init_chores()
+seed_chores()
+print('\n')
+init_roommates()
+seed_roommates()
+
+print('\n\n')
+print("Starting up dashboard...")
+## CREATE CLI dashboard
+cmd = ""
+while cmd != "q":
+    cmd = input("* ")
+    match cmd[0]:
+        case "h":
+            help()
+        case "a":
+            list_all()
+        case "c":
+            list_chores()
+        case "r":
+            list_roommates()
+        case "u":
+            chore_id = cmd[2]
+            roommate_id = cmd[4]
+
+            complete_chore_for_roommate(chore_id, roommate_id)
+
+            chore = get_chore(chore_id)
+            chore_name = chore[1]
+            score = chore[4]
+            person = get_roommate(roommate_id)
+            person_name = person[1]
+
+            print(f"Chore '{chore_name}' updated for roommate '{person_name}' +'{score}'")
+
+    print('\n')
+    
