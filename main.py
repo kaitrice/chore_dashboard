4e99417495bfaaa9631fb6e9565a13c0679f43c9
Kaitlyn Rice <kaitnrice@gmail.com>
2025-09-28 20:35:09 -0700

    init and seed roommates table

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from chores import get_all_chores, init_chores, seed_chores
+from roommates import get_all_roommates, init_roommates, seed_roommates
 
 init_chores()
 seed_chores()
+
+print('\n')
+
+init_roommates()
+seed_roommates()
 
 def print_all_chores():
     rows = get_all_chores()
@@ -15,6 +21,19 @@ def print_all_chores():
         name, frequency, group_id, score = row
         print(f"[Group {group_id}] ({frequency}) {name} | Score: {score}")
 
+def print_all_roommates():
+    rows = get_all_roommates()
+
+    if not rows:
+        print("~ No roommates found")
+        return
+
+    print("\n--- All Roommates ---")
+    for row in rows:
+        name, group_id, score = row
+        print(f"{name} [Group {group_id}] | Score: {score}")
+
 print("---  Chore Dashboard  ---")
 
 print_all_chores()
+print_all_roommates()
---
+++ roommates.py
@@ -0,0 +1,46 @@
+import sqlite3
+from db import get_conn
+
+
+def init_roommates():
+    conn = get_conn()
+    cursor = conn.cursor()
+    cursor.execute('DROP TABLE IF EXISTS roommates;')
+
+    query = """
+        CREATE TABLE roommates (
+            Name VARCHAR(255) NOT NULL,
+            groupId INT NOT NULL,
+            Score INT
+        );
+    """
+
+    cursor.execute(query)
+
+    print("~ Roommates Table initiated")
+
+def insert_roommate(name, groupId):
+    conn = get_conn()
+    cursor = conn.cursor()
+    try:
+        query = "INSERT INTO roommates (name, groupId, score) VALUES (?, ?, 0)"
+        cursor.execute(query, (name, groupId))
+        conn.commit()
+        print(f"'{name}' added successfully.")
+    except sqlite3.IntegrityError:
+        print(f"Error: Chore named '{name}' already exists.")
+
+def get_all_roommates():
+    conn = get_conn()
+    cursor = conn.cursor()
+    query = """
+        SELECT * FROM roommates 
+        ORDER BY groupId
+    """
+    cursor.execute(query)
+    return cursor.fetchall()
+
+def seed_roommates():
+    insert_roommate('Kait', 1)
+    insert_roommate('Zoe', 2)
+    print('~ Roommates seeded')
